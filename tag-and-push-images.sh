7cd63b3e274ae5ddd3b3b13134d6859df4128065
aybolali <asartbai7>
2024-02-26 14:44:32 +0100

    414409

--- tag-and-push-images.sh
+++ tag-and-push-images.sh
@@ -1,12 +1,12 @@
 gcloud auth login
 
-docker tag com.food.ordering.system/order.service:$1 asia-east2-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/order.service:$1
-docker tag com.food.ordering.system/payment.service:$1 asia-east2-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/payment.service:$1
-docker tag com.food.ordering.system/restaurant.service:$1 asia-east2-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/restaurant.service:$1
-docker tag com.food.ordering.system/customer.service:$1 asia-east2-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/customer.service:$1
+docker tag com.food.ordering.system/order.service:$1 asia-east2-docker.pkg.dev/food-ordering-system-414409/food-ordering-system-repository/order.service:$1
+docker tag com.food.ordering.system/payment.service:$1 asia-east2-docker.pkg.dev/food-ordering-system-414409/food-ordering-system-repository/payment.service:$1
+docker tag com.food.ordering.system/restaurant.service:$1 asia-east2-docker.pkg.dev/food-ordering-system-414409/food-ordering-system-repository/restaurant.service:$1
+docker tag com.food.ordering.system/customer.service:$1 asia-east2-docker.pkg.dev/food-ordering-system-414409/food-ordering-system-repository/customer.service:$1
 
-docker push asia-east2-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/order.service:$1
-docker push asia-east2-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/payment.service:$1
-docker push asia-east2-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/restaurant.service:$1
-docker push asia-east2-docker.pkg.dev/food-ordering-system-346904/food-ordering-system-repository/customer.service:$1
+docker push asia-east2-docker.pkg.dev/food-ordering-system-414409/food-ordering-system-repository/order.service:$1
+docker push asia-east2-docker.pkg.dev/food-ordering-system-414409/food-ordering-system-repository/payment.service:$1
+docker push asia-east2-docker.pkg.dev/food-ordering-system-414409/food-ordering-system-repository/restaurant.service:$1
+docker push asia-east2-docker.pkg.dev/food-ordering-system-414409/food-ordering-system-repository/customer.service:$1
 
